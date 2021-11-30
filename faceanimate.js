8ba77ab266a876b10be2f5f9fdc42bada12ac223
unknown <hauspie.guillaume@free.fr>
2021-11-30 23:22:20 +0100

    Irobot speak

---
+++ faceanimate.js
@@ -0,0 +1,16 @@
+<div id="bot" class="neutral">
+<div id="head">
+  <div id="left-ear">
+    <div id="left-ear-inner"></div>
+  </div>
+  <div id="face">
+    <div id="eyes">
+      <div id="left-eye"></div>
+      <div id="right-eye"></div>
+    </div>
+    <div id="mouth"></div>
+  </div>
+  <div id="right-ear">
+    <div id="right-ear-inner"></div>
+  </div>
+</div>
