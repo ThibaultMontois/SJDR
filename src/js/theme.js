cd9a935bdbcbe0c5a3992a958d4e6a1c872f647b
Thibault Montois <thibault.montois@gmail.com>
2021-12-01 17:01:22 +0100

    theme.json

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,28 @@
 let icon_cible = document.getElementById("timeline").firstElementChild.firstElementChild;
 
+/****************************************************************************/
+
+function readJsonFile(file, callback) {
+    let textFile = new XMLHttpRequest();
+    textFile.overrideMimeType("application/json");
+    textFile.open("GET", file, true);
+    textFile.onreadystatechange = function() {
+        if (textFile.readyState === 4 && textFile.status == "200") {
+            callback(textFile.responseText);
+        }
+    };
+    textFile.send(null);
+}
+
+readJsonFile("../src/json/theme.json", function(text) {
+    let data = JSON.parse(text);
+    document.getElementById("recit").innerHTML = data['test'];
+});
+
+/****************************************************************************/
+
 function initiate() {
+    document.getElementsByTagName("main")[0].style.backgroundImage = "url('./img/backgrounds/night.jpg')";
     icon_cible.style.visibility = "visible";
 }
 
