60df434e64d275ce0f0ebd81793a9d4b8e399474
Ghauspie <hauspie.guillaume@free.fr>
2021-12-01 11:45:02 +0100

    function faceanimate.js V1

--- faceanimate.js
+++ faceanimate.js
@@ -1,16 +1,6 @@
-<div id="bot" class="neutral">
-<div id="head">
-  <div id="left-ear">
-    <div id="left-ear-inner"></div>
-  </div>
-  <div id="face">
-    <div id="eyes">
-      <div id="left-eye"></div>
-      <div id="right-eye"></div>
-    </div>
-    <div id="mouth"></div>
-  </div>
-  <div id="right-ear">
-    <div id="right-ear-inner"></div>
-  </div>
-</div>
+function faceanimate(){
+  const recit=document.getElementById("recit")
+  const face='<div id="bot" class="neutral"><div id="head"><div id="left-ear"><div id="left-ear-inner"></div></div><div id="face"><div id="eyes">  <div id="left-eye"></div><div id="right-eye"></div></div><div id="mouth"></div></div><div id="right-ear"><div id="right-ear-inner"></div></div></div>'
+  recit.innerHTML=face;
+}
+
