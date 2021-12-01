4bee576fb2aac4f9eede77844ec4d1def5ad32f2
Ghauspie <hauspie.guillaume@free.fr>
2021-12-01 14:13:29 +0100

    add folder

--- src/js/faceanimate.js
+++ src/js/faceanimate.js
@@ -3,7 +3,6 @@ function faceanimate(){
   const face='<div id="bot" class="neutral"><div id="head"><div id="left-ear"><div id="left-ear-inner"></div></div><div id="face"><div id="eyes">  <div id="left-eye"></div><div id="right-eye"></div></div><div id="mouth"></div></div><div id="right-ear"><div id="right-ear-inner"></div></div></div>'
   recit.innerHTML=face;
   setInterval(removefaceanimate, 10000);
-  /* setTimeout(removefaceanimate(), 100000); */
 }
  function removefaceanimate(){
   const bot=document.getElementById('bot')
