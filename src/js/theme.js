974de44c45678d0f5a78ba68b9785075ab2e184b
Thibault Montois <thibault.montois@gmail.com>
2021-12-02 11:12:42 +0100

    background initiate OK

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,9 @@
+let theme;
+let background;
+let icon;
 let icon_cible = document.getElementById("timeline").firstElementChild.firstElementChild;
 
-/****************************************************************************/
+/************************Ouverture du fichier JSON***************************************/
 
 function readJsonFile(file, callback) {
     let textFile = new XMLHttpRequest();
@@ -15,16 +18,19 @@ function readJsonFile(file, callback) {
 }
 
 readJsonFile("../src/json/theme.json", function(text) {
-    let data = JSON.parse(text);
-    document.getElementById("recit").innerHTML = data['test'];
+    theme = JSON.parse(text);
 });
 
 /****************************************************************************/
 
 function initiate() {
-    document.getElementsByTagName("main")[0].style.backgroundImage = "url('./img/backgrounds/night.jpg')";
+    background = theme.beach.background;
+    icon = theme.beach.icon;
+    document.getElementsByTagName("main")[0].style.backgroundImage = background;
     icon_cible.style.visibility = "visible";
 }
+
+setTimeout(() => initiate(), 100);
 
 document.getElementById("timeline").addEventListener('click', testChangementClick);
 
@@ -33,5 +39,3 @@ function testChangementClick() {
     icon_cible = icon_cible.nextElementSibling;
     icon_cible.style.visibility = "visible";
 }
-
-initiate();
