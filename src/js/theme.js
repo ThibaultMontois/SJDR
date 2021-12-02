cdbea20ed9e3b6759c517d65738c84c98e6ea3e5
Thibault Montois <thibault.montois@gmail.com>
2021-12-02 16:49:44 +0100

    icon transform

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,13 +3,13 @@ let theme;
 let background;
 let icon;
 
-let start_time = "07h00";
-let end_time = "19h00";
-let total_time = 12;
-let actual_time;
+let str_start_time = "07:15";
+let str_end_time = "22:00";
+let str_actual_time = "22:00";
 
-
-let x = 0;
+let start_time;
+let end_time;
+let total_time;
 
 /************************ Ouverture du fichier JSON ***************************************/
 
@@ -17,7 +17,7 @@ function readJsonFile(file, callback) {
     let textFile = new XMLHttpRequest();
     textFile.overrideMimeType("application/json");
     textFile.open("GET", file, true);
-    textFile.onreadystatechange = function() {
+    textFile.onreadystatechange = function () {
         if (textFile.readyState === 4 && textFile.status == "200") {
             callback(textFile.responseText);
         }
@@ -25,22 +25,30 @@ function readJsonFile(file, callback) {
     textFile.send(null);
 }
 
-readJsonFile("../src/json/theme.json", function(text) {
+readJsonFile("../src/json/theme.json", function (text) {
     data = JSON.parse(text);
 });
 
 /****************************************************************************/
 
-function initiate() {
-    loadTheme(tab_test[getRandomInt(8)]);
-    document.getElementsByTagName('main')[0].setAttribute('style', `background-image: url(${background})`);
-    document.getElementsByClassName('timeline_icon')[0].setAttribute('src', icon);
-    document.getElementsByClassName('timeline_icon')[0].setAttribute('style', `transform: translate(${x}%)`);
+function parseTime(str) {
+    let t = str.split(':');
+    return parseInt(t[0]) + parseInt(t[1]) / 60;
 }
 
 function loadTheme(theme) {
     background = data[theme].background;
     icon = data[theme].icon;
+}
+
+function initiate() {
+    start_time = parseTime(str_start_time);
+    end_time = parseTime(str_end_time);
+    total_time = end_time - start_time;
+    loadTheme(tab_test[getRandomInt(8)]);
+    document.getElementsByTagName('main')[0].setAttribute('style', `background-image: url(${background})`);
+    document.getElementsByClassName('timeline_icon')[0].setAttribute('src', icon);
+    document.getElementsByClassName('timeline_icon')[0].setAttribute('style', `transform: translate(${x}%)`);
 }
 
 setTimeout(() => initiate(), 100);
@@ -56,10 +64,14 @@ function getRandomInt(max) {
 document.addEventListener('click', testChangementClick);
 
 function testChangementClick() {
-    let element = document.getElementsByClassName("timeline_icon")[0];
-    x+=100;
+    let timeline_icon = document.getElementsByClassName("timeline_icon")[0];
+    let timeline = document.getElementById("timeline");
+    let time = parseTime(str_actual_time) - start_time;
+    let x = 0.98 * (timeline.offsetWidth - timeline_icon.offsetWidth) * (time / total_time);
+
     loadTheme(tab_test[getRandomInt(8)]);
     document.getElementsByTagName('main')[0].setAttribute('style', `background-image: url(${background})`);
     document.getElementsByClassName('timeline_icon')[0].setAttribute('src', icon);
-    element.setAttribute('style', `transform: translate(${x}%)`);
+
+    timeline_icon.setAttribute('style', `transform: translate(${x}px)`);
 }
