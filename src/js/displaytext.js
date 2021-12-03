fd92d6a6067f090fde2f635ac2b727f0ede4da00
Yohan Lertourné <yohan.lertourne@gmail.com>
2021-12-03 11:12:16 +0100

    adding local storage

--- src/js/displaytext.js
+++ src/js/displaytext.js
@@ -13,6 +13,10 @@ readJsonFile("../src/json/histoire.json", function(text) {
     let data = JSON.parse(text);
     let bot='<div id="head"><div id="left-ear"><div id="left-ear-inner"></div></div><div id="face"><div id="eyes">  <div id="left-eye"></div><div id="right-eye"></div></div><div id="mouth"></div></div><div id="right-ear"><div id="right-ear-inner"></div></div></div>';
     document.getElementById("recit").innerHTML = '<div class="recit"><div id="bot" class="neutral">'+bot+'</div><p class="recit-text">'+data.etape1.histoire+"</p></div><div class='choose'><p id='1' onclick='clickchoix(id)'>"+data.etape1.choix["0"]+"</p><p id='2' onclick='clickchoix(id)'>"+data.etape1.choix["1"]+"</p><p id='3'  onclick='clickchoix(id)'>"+data.etape1.choix["2"]+"</p></div>";
+    let heuredebut = data.journee.debut;
+    let heurefin = data.journee.fin;
+    localStorage.setItem('debut',heuredebut);
+    localStorage.setItem('fin',heurefin);
 });
 
 function clickchoix(id){
