d1bf9373d298403a435f46176c4e434e7736ddbd
Ghauspie <hauspie.guillaume@free.fr>
2021-12-08 11:14:18 +0100

    contact V1.5 and prepare function for send email

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,37 +1,58 @@
 let modal = null;
 
-
 const opendModal= function (e) {
   e.preventDefault(); 
-  let test = e.targert;
-  let fenModal= document.getElementById('dialog');
-  fenModal.setAttribute('aria-modal','true');
-  fenModal.removeAttribute('aria-hidden');
+  let fenModal= document.getElementById('dialog')
+    fenModal.setAttribute('aria-modal','true')
+    fenModal.removeAttribute('aria-hidden')
+    fenModal.style.visibility= 'visible';
 /*   let main= document.querySelector('main') */
-  modal=fenModal;
+;
 /*   main.setAttribute("class","hidden");
   main.removeAttribute('background-image'); */
 /*   modal.addEventListener('click', closeModal);
    modal.getElementById('modal-close').addEventListener('click', closeModal);  */
-  modal.getElementById('dialog').addEventListener('click',stopPropagation);
+ /*   document.getElementById('dialog').removeEventListener('click', opendModal);  */
+  document.getElementById('modal-close').addEventListener('click', closeModal); 
+  document.getElementById('c-dialog').addEventListener('click',stopPropagation);
 }
 
 
 
-const closeModal=function() {
-modal.style.display= 'none'
-modal.setAttribute('aria-hiden', 'true')
-modal.removeAttribute('arial-modal')
+ const closeModal=function(e) {
+  let modal= document.getElementById('dialog');
+  modal.style.visibility= 'hidden';
+  modal.setAttribute('aria-hiden', 'true');
+  modal.removeAttribute('aria-modal');
+  document.getElementById('dialog').removeEventListener('click', opendModal);
+document.getElementById('modal-close').removeEventListener('click', closeModal);
 /* modal.removeEventListener('click', closeModal)
 modal.getElementById('modal-close').removeEventListener('click', closeModal); */
 }
+ 
+  const stopPropagation =function (a) {
+  a.stopPropagation()
+} 
 
-const stopPropagation =function (e) {
-  e.stopPropagation()
-}
- document.getElementById('modal-close').addEventListener('click',closeModal); 
-  document.querySelectorAll('c-dialog').forEach(a => {
-  a.addEventListener('click',openModal)
+ 
+
+  document.querySelectorAll('dialog').forEach(a => {
+  a.addEventListener('click',closeModal)
 })  
+
+
+function sendEmail(){
+  Email.send({
+    To:"hauspie.guillaume@free.fr",
+    From:"hauspie.guillaume@free.fr",
+    Subject:"Question de ",
+    body: document.getElementById('text').value
+  }).then (
+    message =>alert("Votre message es bien envoyé")
+  );
+}
+
 document.getElementById('contact-Modal').addEventListener('click',opendModal);
-document.getElementById('modal-close').addEventListener('click',closeModal);
+document.getElementById('modal-close').addEventListener('click',closeModal); 
+
+
